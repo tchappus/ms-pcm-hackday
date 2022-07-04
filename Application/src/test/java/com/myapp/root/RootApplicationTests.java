0937807f4c1af5c1856d8b69807b88c2d1787450
Trent Chappus <trentjameschap@icloud.com>
2022-07-04 16:31:51 -0400

    change junit version

--- Application/src/test/java/com/myapp/root/RootApplicationTests.java
+++ Application/src/test/java/com/myapp/root/RootApplicationTests.java
@@ -1,12 +1,10 @@
 package com.myapp.root;
 
-import static org.junit.Assert.assertTrue;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import static org.springframework.test.util.AssertionErrors.assertTrue;
+
 @SpringBootTest
 public class RootApplicationTests {
 
