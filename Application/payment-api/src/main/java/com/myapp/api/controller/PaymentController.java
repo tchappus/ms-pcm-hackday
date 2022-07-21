e119fb282f7e79391c54f85771749755702f3ca7
Trent Chappus <trentjameschap@icloud.com>
2022-07-20 21:19:06 -0400

    Add deployment step for API

--- Application/payment-api/src/main/java/com/myapp/api/controller/PaymentController.java
+++ Application/payment-api/src/main/java/com/myapp/api/controller/PaymentController.java
@@ -5,19 +5,15 @@ import java.util.function.Consumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.azure.cosmos.ChangeFeedProcessor;
 import com.azure.cosmos.ChangeFeedProcessorBuilder;
-import com.azure.cosmos.CosmosAsyncClient;
 import com.azure.cosmos.CosmosAsyncContainer;
 import com.azure.cosmos.CosmosAsyncDatabase;
-import com.azure.cosmos.CosmosClient;
 import com.azure.cosmos.CosmosClientBuilder;
 import com.azure.cosmos.models.ChangeFeedProcessorOptions;
 import com.azure.spring.data.cosmos.CosmosFactory;
@@ -40,8 +36,8 @@ public class PaymentController {
     public static ChangeFeedProcessor getChangeFeedProcessor(String hostName, CosmosAsyncContainer feedContainer, CosmosAsyncContainer leaseContainer, Consumer<EnrichedPayment> paymentConsumer) {
 
         var options = new ChangeFeedProcessorOptions();            
-        options.setStartFromBeginning(false);
-        options.setLeasePrefix("myChangeFeedDeploymentUnit");
+        options.setStartFromBeginning(true);
+        options.setLeasePrefix("apiSubscription");
         
         return new ChangeFeedProcessorBuilder()
                 .hostName(hostName)
@@ -49,26 +45,16 @@ public class PaymentController {
                 .feedContainer(feedContainer)
                 .leaseContainer(leaseContainer)
                 .handleChanges((List<JsonNode> docs) -> {
-                    logger.info("--->setHandleChanges() START");
-    
+
                     for (JsonNode document : docs) {
                         try {
-                            //Change Feed hands the document to you in the form of a JsonNode
-                            //As a developer you have two options for handling the JsonNode document provided to you by Change Feed
-                            //One option is to operate on the document in the form of a JsonNode, as shown below. This is great
-                            //especially if you do not have a single uniform data model for all documents.
-                            logger.info("---->DOCUMENT RECEIVED: " + OBJECT_MAPPER.writerWithDefaultPrettyPrinter()
-                                    .writeValueAsString(document));
-    
-                            //You can also transform the JsonNode to a POJO having the same structure as the JsonNode,
-                            //as shown below. Then you can operate on the POJO.
-                            EnrichedPayment pojo_doc = OBJECT_MAPPER.treeToValue(document, EnrichedPayment.class);
-                            logger.info("----=>id: " + pojo_doc.getId());
+                            EnrichedPayment payment = OBJECT_MAPPER.treeToValue(document, EnrichedPayment.class);
+                            logger.info("----=>new payment - id: " + payment.getId());
 
-                            paymentConsumer.accept(pojo_doc);
+                            paymentConsumer.accept(payment);
     
                         } catch (JsonProcessingException e) {
-                            e.printStackTrace();
+                            logger.error("Error processing object", e);
                         }
                     }
                     logger.info("--->handleChanges() END");
@@ -89,18 +75,17 @@ public class PaymentController {
             var feedContainer = db.getContainer("payments");
             var leaseContainer = db.getContainer("payments-lease");
 
-            logger.info("-->START Change Feed Processor on worker (handles changes asynchronously)");
+            logger.info("-->New subscriber");
 
             Consumer<EnrichedPayment> paymentConsumer = payment -> {
                 emitter.next(payment);
             };
 
-            var changeFeedProcessorInstance = getChangeFeedProcessor("SampleHost_1", feedContainer, leaseContainer, paymentConsumer);
+            var changeFeedProcessorInstance = getChangeFeedProcessor("payments-api-host", feedContainer, leaseContainer, paymentConsumer);
             changeFeedProcessorInstance.start()
             .subscribeOn(Schedulers.boundedElastic())
             .doOnSuccess(aVoid -> {
-                //pass
-                logger.info("subscribed");
+                logger.info("New subscription complete");
             })
             .subscribe();
         });
