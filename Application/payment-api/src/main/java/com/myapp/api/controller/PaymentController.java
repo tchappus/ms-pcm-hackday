4ba9301d9d5684f6a60cb0189e72584dc9efd233
Trent Chappus <trentjameschap@icloud.com>
2022-07-23 20:03:20 -0400

    api endpoint for testing ui

--- Application/payment-api/src/main/java/com/myapp/api/controller/PaymentController.java
+++ Application/payment-api/src/main/java/com/myapp/api/controller/PaymentController.java
@@ -1,5 +1,6 @@
 package com.myapp.api.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 import java.util.function.Consumer;
@@ -22,6 +23,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.myapp.lib.EnrichedPayment;
+import com.myapp.lib.Party;
 
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
@@ -95,5 +97,56 @@ public class PaymentController {
             emitter.onDispose(() -> changeFeedProcessorInstance.stop());
         });
     }
-    
+
+
+    @CrossOrigin(allowedHeaders = "*")
+    @GetMapping(value = "/payments-test", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<EnrichedPayment> getPaymentsTest() {
+        return Flux.<EnrichedPayment>create(emitter -> {
+
+            int x = 1;
+            long y = (long)(Math.sin(x) + 0.5 * x + Math.cos(x));
+            while (true) {
+                x++;
+
+                long y2 = (long)(Math.sin(x) + 0.5 * x + Math.cos(x));
+
+                long amount = y2 - y;
+
+                y = y2;
+
+                var dir = "into";
+                if (amount < 0) {
+                    dir = "outfr";
+                }
+
+                EnrichedPayment payment = new EnrichedPayment();
+                payment.setAmount(Math.abs(amount));
+                payment.setCurrency("USD");
+                payment.setDirection(dir);
+                payment.setInternalParty(new Party());
+                payment.getInternalParty().setBankName("BANK");
+                payment.getInternalParty().setBicCode("MSNYUS33");
+                payment.getInternalParty().setBranch("branch");
+                payment.getInternalParty().setCity("city");
+
+                payment.setExternalParty(new Party());
+                payment.getExternalParty().setBankName("bankName");
+                payment.getExternalParty().setBicCode("bicCode");
+                payment.getExternalParty().setBranch("branch");
+                payment.getExternalParty().setCity("city");
+
+                payment.setTimestamp(LocalDateTime.now());
+                payment.setId(UUID.randomUUID().toString());
+                emitter.next(payment);
+                
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 }
